e5113bd4da98244272855ced0315f05557d0a680
cartercameron1 <cartercameron864@gmail.com>
2023-11-09 22:15:27 -0600

    sample user

--- server/users/views.py
+++ server/users/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
-# Create your views here.
 
-sample_user = {'id': 69, 'name': 'A.S. Muncher'}
+sample_user = {
+	"id": 69,
+	"name": "A.S Muncher",
+	"gymID": 420,
+	"DOB": 70,
+	"sex": "male",
+	"height": 80,
+	"lifts": {
+		"bench": 585,
+		"squat": 952,
+		"deadlift": 139
+	}
+}
 
 def sample(request):
+
     return JsonResponse(sample_user)
+
